93a37d27bf935154f46924155c16923847042c8c
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2020-10-10 20:48:23 +0800

    Grey out unused buttons

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -16,10 +16,10 @@ export const Navigation: React.VFC<NavigationProps> = ({ visibility, totalDispla
         <div id="navigation" style={visibility ? {} : { visibility: "hidden" }}>
             <button className="navigationButton" id="setMatchButton" onClick={() => setTotalDisplayState({ ...totalDisplayState, playerEntryView: true })}>Set Match</button>
             <button className="navigationButton" id="resetMatchButton" onClick={() => ResetCurrentMatch()}>Reset All</button>
-            <button className="navigationButton" id="importButton">Import</button>
-            <button className="navigationButton" id="fixturesButton">Fixtures</button>
-            <button className="navigationButton" id="prevButton">{"<<"}</button>
-            <button className="navigationButton" id="nextButton">{">>"}</button>
+            <button className="navigationButton" id="importButton" disabled style={{ opacity: 0.5 }}>Import</button>
+            <button className="navigationButton" id="fixturesButton" disabled style={{ opacity: 0.5 }}>Fixtures</button>
+            <button className="navigationButton" id="prevButton" disabled style={{ opacity: 0.5 }}>{"<<"}</button>
+            <button className="navigationButton" id="nextButton" disabled style={{ opacity: 0.5 }}>{">>"}</button>
         </div>
     )
 };
